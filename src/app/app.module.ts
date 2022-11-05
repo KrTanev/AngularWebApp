aec0a399586065d0e285bfe72a33a392217334e7
KrTanev <kr.tanev01@gmail.com>
2022-11-05 15:58:02 +0200

    added working footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 
 @NgModule({
-  declarations: [AppComponent, MainComponent, HeaderComponent],
+  declarations: [AppComponent, MainComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent, MainComponent, HeaderComponent],
+  bootstrap: [AppComponent, MainComponent, HeaderComponent, FooterComponent],
 })
 export class AppModule {}
