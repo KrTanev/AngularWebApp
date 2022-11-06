7eca55e154693aa33bb509f2ff2fa34059f1b3d4
KrTanev <kr.tanev01@gmail.com>
2022-11-06 21:49:08 +0200

    implemented card requsts -> CRUD

--- src/app/card-list/card-list.component.ts
+++ src/app/card-list/card-list.component.ts
@@ -10,32 +10,55 @@ import { Post } from '../post.interface';
 })
 export class CardListComponent implements OnInit, OnDestroy {
   posts?: Post[];
+  selectedPost: Post;
 
   destroy$ = new Subject<boolean>();
 
-  constructor(private cardsService: CardsService) {}
+  constructor(private cardsService: CardsService) {
+    this.selectedPost = {
+      title: '',
+      content: '',
+      author: '',
+      publishDate: '',
+    };
+  }
 
   ngOnInit(): void {
-    this.cardsService
-      .getPosts()
-      .pipe(takeUntil(this.destroy$))
-      .subscribe(
-        (response) => {
-          this.posts = response;
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
+    this.getData();
   }
 
   onPostSubmit(post: Post): void {
-    const newPost = {
-      ...post,
-      id: this.posts?.length,
-    };
+    if (post.id) {
+      this.cardsService
+        .updatePosts({ ...post })
+        .pipe(takeUntil(this.destroy$))
+        .subscribe(() => this.getData());
 
-    this.posts?.push(newPost);
+      return;
+    } else {
+      this.cardsService
+        .createPosts(post)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe(() => this.getData());
+    }
+  }
+
+  onPostSelect(post: Post): void {
+    this.selectedPost = post;
+  }
+
+  onPostDelete(postId: number): void {
+    this.cardsService
+      .deletePosts(postId)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => this.getData());
+  }
+
+  private getData(): void {
+    this.cardsService
+      .getPosts()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((response) => (this.posts = response));
   }
 
   ngOnDestroy(): void {
