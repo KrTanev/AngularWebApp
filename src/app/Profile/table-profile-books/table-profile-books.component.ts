fb3440f0588a39bb9fd73574f0e6fd30f19957b7
KrTanev <kr.tanev01@gmail.com>
2022-11-12 01:43:43 +0200

    added table books component for profile

---
+++ src/app/Profile/table-profile-books/table-profile-books.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-profile-books',
+  templateUrl: './table-profile-books.component.html',
+  styleUrls: ['./table-profile-books.component.scss']
+})
+export class TableProfileBooksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
