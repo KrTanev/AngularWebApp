8d38a743e9b014b1793e3204ab65cf54a3487929
KrTanev <kr.tanev01@gmail.com>
2022-11-12 02:21:32 +0200

    profile-books component works

--- src/app/Profile/table-profile-books/table-profile-books.component.ts
+++ src/app/Profile/table-profile-books/table-profile-books.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { take } from 'rxjs';
+import { User } from '../../auth/user.model';
+import { Post } from '../../post.interface';
+import { PostsService } from '../../postService';
 
 @Component({
   selector: 'app-table-profile-books',
   templateUrl: './table-profile-books.component.html',
-  styleUrls: ['./table-profile-books.component.scss']
+  styleUrls: ['./table-profile-books.component.scss'],
 })
 export class TableProfileBooksComponent implements OnInit {
+  @Input() user: User;
 
-  constructor() { }
+  myPosts: Post[];
+
+  constructor(private postsService: PostsService) {}
 
   ngOnInit(): void {
+    this.getData();
   }
 
+  getData(): void {
+    this.postsService
+      .getPosts()
+      .pipe(take(1))
+      .subscribe((response) => {
+        this.getUserPosts(response);
+      });
+  }
+
+  getUserPosts(posts: Post[]): void {
+    const myPosts: Post[] = [];
+    posts.forEach((post) => {
+      post.addedBy === this.user.username && myPosts.push(post);
+    });
+    this.myPosts = myPosts;
+  }
+
+  onDeleteClick(postId?: number): void {
+    this.postsService
+      .deletePosts(postId || 0)
+      .pipe(take(1))
+      .subscribe(() => this.getData());
+  }
 }
